3aa1e949e67cf96c4d1cbc904d9b7f1aab90849d
Ivan <vanya1254@yandex.ru>
2024-01-31 14:35:59 +0300

    Updates

---
+++ static/js/About.3e6f3dc2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_rick_and_morty=self.webpackChunkreact_rick_and_morty||[]).push([[74],{2026:(e,a,r)=>{r.r(a),r.d(a,{default:()=>t});r(2791);var s=r(184);const t=()=>(0,s.jsx)("section",{className:"bottom__wrapper",children:(0,s.jsx)("div",{className:"bottom__inner"})})}}]);
+//# sourceMappingURL=About.3e6f3dc2.chunk.js.map
